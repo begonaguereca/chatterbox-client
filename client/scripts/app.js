2a16b2613f4775d32a5aa082105c4228a9cd7c28
begonaguereca <begonaguereca@gmail.com>
2017-10-20 15:45:15 -0700

    Some kinda progress~!

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,56 @@
-// YOUR CODE HERE:
+var app = {};
+
+app.init = function() {
+
+};
+
+app.send = function (data) {
+  $.ajax({
+    url: 'http://parse.sfm6.hackreactor.com/',
+    type: 'POST',
+    data: data,
+    success: function() {
+      console.log('Success!!');
+    }, 
+    error: function() {
+      console.log('ERROR');
+    }
+  });
+};
+
+app.fetch = function (informationLocation) {
+  $.ajax({
+    url: informationLocation,
+    type: 'GET',
+    success: function() {
+      console.log('Success!!');
+    }, 
+    error: function() {
+      console.log('ERROR');
+    }
+  });
+};
+
+app.clearMessages = function() {
+  //$('#chats').html('');
+  document.getElementById('chats').innerHTML = '';
+};
+
+app.renderMessage = function(message) {
+  //debugger
+  var newNode = document.createElement(message);
+  // console.log (document.getElementById('chats').children);
+  console.log($('#chats'));
+  document.getElementById('chats').appendChild(newNode);
+};
+
+app.renderMessage ({
+  username: 'Mel Brooks',
+  text: 'Never underestimate the power of the Schwartz!',
+  roomname: 'lobby'
+});
+
+app.renderRoom = function(room) {
+  
+};
+
