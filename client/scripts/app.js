2253a7f506b4ef4ec3e8f11b4cd7ec7a0d5faf2f
begonaguereca <begonaguereca@gmail.com>
2017-10-20 21:57:54 -0700

    loading messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,56 +1,87 @@
 var app = {};
 
 app.init = function() {
-
 };
 
 app.send = function (data) {
+  var newData = {
+  createdAt: null,
+  objectId: ,
+  roomname: ,
+  text: data,
+  updatedAt: ,
+  username: newSearch
+  };
+
   $.ajax({
-    url: 'http://parse.sfm6.hackreactor.com/',
+    url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
     type: 'POST',
-    data: data,
+    contentType: 'application/json',
+    data: JSON.stringify(data),
     success: function() {
-      console.log('Success!!');
+      console.log(`You have successfully posted ${data} to the server`);
     }, 
     error: function() {
-      console.log('ERROR');
+      console.log('Error, the server was unable to handle your request');
     }
   });
 };
 
-app.fetch = function (informationLocation) {
+app.fetch = function () {
   $.ajax({
-    url: informationLocation,
+    url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
     type: 'GET',
-    success: function() {
-      console.log('Success!!');
+    contentType: 'application/json',
+    success: function(element) {
+      console.log (element);
+      element.results.forEach (function(obj) {
+        //button 
+        
+
+        //roomName 
+
+
+        //message
+        $('#chats').append(`<p>${obj.username}</p>
+          <p>${obj.text}</p>`);
+
+      });
     }, 
     error: function() {
-      console.log('ERROR');
+      console.log('ERROR herree');
     }
   });
 };
 
 app.clearMessages = function() {
-  //$('#chats').html('');
-  document.getElementById('chats').innerHTML = '';
+  $('#chats').html('');
+  // document.getElementById('chats').innerHTML = '';
 };
 
 app.renderMessage = function(message) {
-  //debugger
-  var newNode = document.createElement(message);
-  // console.log (document.getElementById('chats').children);
-  console.log($('#chats'));
-  document.getElementById('chats').appendChild(newNode);
+  console.log ('chats: ', $('#chats'));
 };
 
-app.renderMessage ({
-  username: 'Mel Brooks',
-  text: 'Never underestimate the power of the Schwartz!',
-  roomname: 'lobby'
+app.renderRoom = function(roomName) {
+  console.log ('room: ', $('#roomSelect'));
+  $('#roomSelect').html(`<option>${roomName}</option>`);
+};
+
+app.handleUsernameClick = function () {
+  return true;
+};
+
+// $('#chats').append (app.fetch ());
+app.fetch ();
+
+$('#sharing').click(function() {
+  app.send($('#usertext'));
+
+
 });
 
-app.renderRoom = function(room) {
-  
-};
+
+// app.fetch ();
+
+
 
